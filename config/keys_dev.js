56b3f13d754f1f42b5b212212de93e9bbdbe20b7
Vuthy <suonx002@gmail.com>
2019-12-06 01:09:56 -0600

    fixed untrack

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://vuthy:lVoYVzro6jzCdzSa@slickshop-izhls.mongodb.net/slickShop?retryWrites=true&w=majority',
-  jwtSecret: 'secrettokenformyserver'
-};
